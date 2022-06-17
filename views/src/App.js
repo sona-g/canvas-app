8a6711d94e9c3f7e737ef3906af3bb22a7ac30c7
sonakshi <sonakshigurejja20@gmail.com>
2022-06-17 11:59:47 +0800

    added routes

--- views/src/App.js
+++ views/src/App.js
@@ -1,12 +1,19 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './components/Header'
+import Main from './pages/Main';
+import Login from './pages/Login';
+import Post from './pages/Post'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        <Header />
       <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path="/user" element={<Login />}/>
+        <Route path="/posts" element={ <Main />} />
+        <Route path="/posts/:id" element={<Post />} />
       </Routes>
       </BrowserRouter>
     </div>
