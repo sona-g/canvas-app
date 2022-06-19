0d020b8f9b472409c64c1d684e6ace014268cce0
Brandon Yeo <branyzp@34.8.126.129.static.m1net.com.sg>
2022-06-19 19:17:51 +0800

    small updates to css lol

--- views/src/components/Header.jsx
+++ views/src/components/Header.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
+	const [searchQuery, setSearchQuery] = useState('');
+
+	const handleSearch = (e) => {
+		setSearchQuery(e.target.value);
+		console.log(searchQuery);
+	};
+
 	return (
 		<div className="NavBar">
 			<Link to="/">
-				<img id="logo" src="https://i.imgur.com/oqfbc6g.png" alt="logo" />
+				<img id="logo" src="https://i.imgur.com/dXjWbR1.png" alt="logo" />
 			</Link>
-			<Link className="NavBarLink" to="user">
-				Login
+			<Link className="NavBarLink" to="/user">
+				Login / Sign Up
 			</Link>
-			<Link className="NavBarLink" to="posts">
+			<Link className="NavBarLink" to="/posts">
 				Main
 			</Link>
-			<Link className="NavBarLink" to="posts/:id">
-				selectedPost
+			<Link className="NavBarLink" to="/posts/:id">
+				selectedPost placeholder
 			</Link>
+
+			<input
+				id="search"
+				name="search"
+				type="search"
+				placeholder="search for a post"
+				onChange={handleSearch}
+			/>
 		</div>
 	);
 };
