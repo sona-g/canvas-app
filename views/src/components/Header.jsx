4d4f457df6ea4f0887494432db2862239e16093e
Brandon Yeo <branyzp@34.8.126.129.static.m1net.com.sg>
2022-06-19 18:20:04 +0800

    updated NavBar and minor CSS

--- views/src/components/Header.jsx
+++ views/src/components/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <div>
-            navbar
-        </div>
-    );
+	return (
+		<div className="NavBar">
+			<Link to="/">
+				<img id="logo" src="https://i.imgur.com/oqfbc6g.png" alt="logo" />
+			</Link>
+			<Link className="NavBarLink" to="user">
+				Login
+			</Link>
+			<Link className="NavBarLink" to="posts">
+				Main
+			</Link>
+			<Link className="NavBarLink" to="posts/:id">
+				selectedPost
+			</Link>
+		</div>
+	);
 };
 
 export default Header;
